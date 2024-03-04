71f84c090c78abd99bf406869c5f72eb7452e3af
silva-vitor <vitor.p.silva@iesb.edu.br>
2024-03-04 08:53:28 -0300

    atualização

--- COSTRUÇÃO-BACKEND/fundamento-aula02.js
+++ COSTRUÇÃO-BACKEND/fundamento-aula02.js
@@ -1,45 +1,21 @@
-//dfghjkl
+//comentario de uma linha 
 
-/*kfjfgfelkgeflg
-lfgfllflgldfgldfflgglf*/
- var nome = "vitor"
+/* 
+comentario de varias linhas 
+tudo na vida tem um inicio e um fim
+todo dia e dia de programação
+*/ 
 
-// imprimir informação  no consle (terminal)
-console.log("imprimindo um texto ")
+//variável com var
+var nome = "vitor"
 
+//imprimier informação no console(terminal)
+
+console.log("IMPRIMINDO UM TEXTO")
 console.log(2)
 console.log(nome)
 console.log(1,2,3,4)
-console.log("nome:", "vitor")
-console.log("nome", nome)
-console.log("qualquer texto", "qualquer outro texto")
-console.log(2 +2 )
-
-//tipo de dados
-
-var texto = "Teste"
-console.log("tipo de variavel de texto", typeof texto)
-
-var numero =2
-console.log( "tipo de variavel numero", typeof numero)
-
-var numeroFlutuante = 2.000
-console.log("tipo de variavel numerosFlutuante", typeof numeroFlutuante)
-
-var numeroNegativo = -2.9999
-console.log("tipo de variavel numeroNegativo:", typeof numeroNegativo)
-
-var boleano  = true // ou false
-console.log( "tipo de variavel boleano", typeof boleano)
-
-var array = [1,2,3,4]
-console.log( "tipo de variavel array", typeof array)
-
-var testeAlteracao = "um texto"
-testeAlteracao = 2 
-console.log ("tipo de variavel testeAleatorio ", typeof testeAleatorio)
-
-var arrayNumero = [1,2,3,4]
-var arraycarro =["gol", "palio", "strada"]
-var arrayDinamica =[ 1, "nome", true,]
-
+console.log("NOME",Vítor)
+console.log("NOME",nome)
+console.log("qualquer texto" + "qualquer outro texto")
+console.log(2 +2)
