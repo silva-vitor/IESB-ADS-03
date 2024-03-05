3b026e92f8e2a2e560882b386681ae70cd3ffb26
silva-vitor <vitor.p.silva@iesb.edu.br>
2024-03-04 21:30:19 -0300

    fundamentos

---
+++ COSTRUÇÃO-BACKEND/fundamentos-aula03.js
@@ -0,0 +1,27 @@
+//comentario de uma linha
+
+/* comentário de várias linhas (em bloco)
+
+*/
+
+//Inpresão de dados no console (terminal)
+console.log("testando impressão no terminal")
+
+console.error("testando erro")
+
+console.warn("testando console.warn")
+
+//criando variáveis com var
+var nome;
+nome = "vitor";
+nome="vitor silva";
+
+console.log("imprimindo variável nome: ")
+console.log(nome)
+
+// let e cost
+
+//cost (costante)
+//cria variaveis que não podem ser alteradas
+
+
