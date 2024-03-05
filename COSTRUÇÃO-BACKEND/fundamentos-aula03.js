24e781146a5f7dc81b4cff8af886f36b04947896
silva-vitor <vitor.p.silva@iesb.edu.br>
2024-03-05 00:24:44 -0300

    revisão javascript

--- COSTRUÇÃO-BACKEND/fundamentos-aula03.js
+++ COSTRUÇÃO-BACKEND/fundamentos-aula03.js
@@ -23,5 +23,51 @@ console.log(nome)
 
 //cost (costante)
 //cria variaveis que não podem ser alteradas
+const primeiroNome = "vitors"
+//primeiroNome ="carlos" //Não posso reatribuir variáveis constantes
+
+console.log(primeiroNome)
+
+//let
+// criar variaveis que podem ser alteradas
+
+let idade
+idade =20
+idade = 30
+idade= "trinta"
+
+console.log("imprimindo o valor da variavel idade (com let)")
+console.log(idade)
+
+//Tipo de dados
+//string
+
+let nomeCompleto = "vitor pereira da silva "
+nomeCompleto='vitor pereira da silva '
+
+//let textoOla= "ola" + nomeCompleto +",tudo bem com você ?"+ "qual o seu nome ?"+ idade + "anos"
+let textoOlaCOmplateString = `ola ${nomeCompleto}, tudo bem com você ?` // template string
+
+console.log(textoOlaCOmplateString)
+
+let nomeUsuario = "vitor silva"
+let idadeUsuario = 23
+let cusoUsuario = "analise e desenvolvimento de sistemas"
+
+//template string
+let pessoaComTemplateString = `
+Olá ${nomeUsuario},
+sua idade é ${idadeUsuario},
+e seu curso é ${cusoUsuario}
+no IESB 
+`
+
+console.log(pessoaComTemplateString)
+
+
+
+
+
+
 
 
