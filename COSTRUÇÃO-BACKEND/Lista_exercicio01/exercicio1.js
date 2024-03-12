7c4dace33b1b7ec92d042f9493c8a056d20229b1
Vitor Silva <131622373+silva-vitor@users.noreply.github.com>
2024-03-12 13:11:02 -0300

    Atualizar o exercicio1.js

--- COSTRUÇÃO-BACKEND/Lista_exercicio01/exercicio1.js
+++ COSTRUÇÃO-BACKEND/Lista_exercicio01/exercicio1.js
@@ -1,11 +1,11 @@
-let prompt = require('prompt-sync') ();
+const prompt = require('prompt-sync') ();
 
-let nota1 = Number (prompt("Nota da P1 ?"));
-let nota2 = Number (prompt("Nota da P2 ?"));
-let nota3 = Number (prompt("Nota da P3 ?"));
-let nota4 = Number (prompt("Nota da P4 ?"));
+const nota1 = Number (prompt("Nota da P1 ?"));
+const nota2 = Number (prompt("Nota da P2 ?"));
+const nota3 = Number (prompt("Nota da P3 ?"));
+const nota4 = Number (prompt("Nota da P4 ?"));
 
-let media = (nota1 + nota2 + nota3 + nota4) / 4
+const media = (nota1 + nota2 + nota3 + nota4) / 4
 console.log("Media: ", media)
 if (media >=7){
    console.log ("APROVADO ")
