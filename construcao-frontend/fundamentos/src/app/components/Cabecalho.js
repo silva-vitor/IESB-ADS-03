0487f24946802606c4df38461e40fbf8d8d48dd3
silva-vitor <vitor.p.silva@iesb.edu.br>
2024-08-15 21:49:38 -0300

    at

---
+++ construcao-frontend/fundamentos/src/app/components/Cabecalho.js
@@ -0,0 +1,15 @@
+ export default function Cabecalho(){
+
+    return(
+
+        <>
+            <div>
+                <h1>Cabeçalho</h1>
+                <p>Isto e o cabeçalho da monha pagina</p>
+
+            </div>
+        </> 
+        
+    )
+}
+   
