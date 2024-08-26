87317274476c9283bfa7961fdb0b771e64614e6d
Vitor pereira <vitor.p.silva@iesb.edu.br>
2024-08-26 19:24:31 -0300

    at

--- construcao-frontend/atividade02/src/app/components/Pagina.js
+++ construcao-frontend/atividade02/src/app/components/Pagina.js
@@ -7,8 +7,8 @@ export default function Pagina(props) {
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="">Home</Nav.Link>
-                        <Nav.Link href="carros">Carros</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/carros">Carros</Nav.Link>
                        
                     </Nav>
                 </Container>
