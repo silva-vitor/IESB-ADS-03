a6f3db1e81c1e046c8830c98e161e87f249b0659
vitor <vp986788@gmail.com>
2024-08-29 13:44:28 -0300

    atividade 01

--- construcao-frontend/fundamentos/src/app/numeros/page.js
+++ construcao-frontend/fundamentos/src/app/numeros/page.js
@@ -1,36 +1,24 @@
 'use client'
 
 import Pagina from "@/app/components/Pagina";
-
-import React, { useState } from 'react';
-
-export default function Contador() {
-    // Estado para armazenar o número
-    const [numero, setNumero] = useState(0);
-
-    // Função para incrementar o número
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+export default function Page() {
+    const [numero, setNumero] = useState('0')
     const Almentar = () => {
-        setNumero(numero + 1);
+       setNumero(numero + 1);
+        
     };
-
-    // Função para decrementar o número
-    const Diminuir = () => {
+    const diminuir = () => {
         setNumero(numero - 1);
     };
-
     return (
-        <Pagina titulo= "Numeros">
-            <div style={{ textAlign: 'center', marginTop: '50px' }}>
-
-
-                <div>
-                    <button onClick={Almentar} style={{ marginRight: '10px', padding: '10px 20px' }}>-</button>
-
-                    <h2>{numero}</h2>
-                    
-                    <button  onClick={Diminuir} style={{ padding: '10px 20px' }}>+</button>
-                </div>
-            </div>
+        <Pagina titulo="Numeros">
+  <Button onClick={Almentar}>+</Button>
+            <h1>{numero}</h1>
+            <Button onClick={diminuir}>-</Button>
         </Pagina>
-    );
+
+
+    )
 }
