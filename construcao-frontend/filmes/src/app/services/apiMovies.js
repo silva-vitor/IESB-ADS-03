3ffb8ba733f8dfc68f27ec598762b142f1767c3f
silva-vitor <vitor.p.silva@iesb.edu.br>
2024-09-08 10:18:06 -0300

    atu

--- construcao-frontend/filmes/src/app/services/apiMovies.js
+++ construcao-frontend/filmes/src/app/services/apiMovies.js
@@ -6,8 +6,7 @@ const apiMovie = axios.create({
         language: 'pt-BR',
     } ,
     headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NGM5ODk1MzZlODRkZjhmNGU5YTAxZjYxMjFkMDA5NiIsIm5iZiI6MTcyNTgwMTMyOS4zMTE0NTcsInN1YiI6IjY2ZDBmODE1OWRjNzNhODdhMmIwNGM3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wOw6LQq2qSxMg9G8zqssHI_MaqMCYyG5YEMxiL3K7GA' 
-
+        Authorization: 'Bearer '
     }
 })
 
